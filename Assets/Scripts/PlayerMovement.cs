d4e2275d32a62b89254cc63b3c7ca2f1805d11bc
Gohar ayub <anoynomousgamez@gmail.com>
2023-01-11 22:28:57 +0500

    Jump added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,29 +8,44 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float _playerJumpVelocity = 10f;
 
     private bool isGrounded = false;
-    public Transform groundCheck;
-    public float groundCheckRadius;
+    public float raycastDistance = 0.1f;
     public LayerMask whatIsGround;
 
     private Rigidbody rb;
+
+    [SerializeField] Transform player;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-
     void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
-    }
+        isGrounded = Physics.Raycast(player.position, Vector3.down, raycastDistance, whatIsGround);
+        Debug.DrawRay(player.position, Vector3.down * raycastDistance, Color.red);
 
-    void Update()
-    {
-        this.transform.Translate(Vector3.right * _playerSpeed * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(isGrounded)
         {
-            rb.AddForce(new Vector2(0f, _playerJumpVelocity), ForceMode.Impulse);
-            isGrounded = false;
+            isGrounded = true;
         }
     }
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            Debug.Log("jUMPING");
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * _playerJumpVelocity, ForceMode.Impulse);
+        isGrounded = false;
+    }
+    void Movement()
+    {
+        this.transform.Translate(Vector3.right * _playerSpeed * Time.deltaTime);
+    }
 }
+
